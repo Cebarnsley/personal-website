8a90035fdb09ab9808699e65993c21324ca04b9c
Candice Barnsley <cebarnsley@gmail.com>
2021-03-02 18:57:39 +0000

    changed link adress

--- src/pages/components/ContactContent/contactContent.js
+++ src/pages/components/ContactContent/contactContent.js
@@ -8,7 +8,7 @@ export default function ContactContent() {
         <p><span style={{color: "rgb(86, 196, 181)"}}>Phone: </span>07577222750</p>
         <p><span style={{color: "rgb(86, 196, 181)"}}>Email: </span>cebarnsley@gmail.com</p>
             <div className="contactLinks">
-                <p><a  href="linkedin.com/in/candice-barnsley-9a574b18a">LinkedIn</a></p>
+                <p><a  href="https://linkedin.com/in/candice-barnsley-9a574b18a">LinkedIn</a></p>
                 <p><a href="https://github.com/Cebarnsley">GitHub</a></p>
             </div>
     </div>
